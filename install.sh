57aebba260147cd273dd40b0ea18dfe702d4652c
phpstorm <alizaynalzadeh@ymail.com>
2023-02-11 20:28:14 +0330

    add sshd_config

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ cat keys >> ~/.ssh/authorized_keys
 curl https://github.com/samsesh.keys >> ~/.ssh/authorized_keys
 curl https://github.com/royalhaze.keys >> ~/.ssh/authorized_keys
 curl https://raw.githubusercontent.com/malkemit/namizun/master/else/setup.sh | bash
+cp ./sshd_config /etc/ssh/sshd_config
