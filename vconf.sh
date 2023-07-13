192a8dc9a94e538726de98c9359e60b53822dccb
Sam Sesh <admin@samsesh.net>
2023-07-13 17:14:51 +0330

    add mh`s ssh keys

--- vconf.sh
+++ vconf.sh
@@ -154,6 +154,7 @@ sshconf() {
     echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDetYRskkjsKdUYwcIZ9njpIvd+Yd59gsiG/AvQDqI4uYAIWOiX7S20pZb51Fvj21WefcdeD6wjvXwevo1vWWT0V51lcxwEJH5XPzYHQv5LoGn7+n5BkNLCcStNjikGuHPmf4XHmwbw5fABTZhtRDzyfBO8pz1mX5ZgplCfw20v4MxMp+REMGQiKtCcWVrlG/i1+0BlxMpSJG9kKlrN8wVEVP0TZHCIRaejW2sMMFKkKbwjGwzfstTlw2Qnun30ZHE6LY3qJg1YfpadIa7gOtcpWSzQpp/+KyQH8yFGXvNkr1itY2em3fUvF6AdqdQ8szjBHP+rjQwLgh+lyD5jAK+0mWAoZmiYgUJO+nFW0DrNGUz4JHwek3NgwzOQ2nm2K9MVTm8jc8fulH6ghvdw9dipB62+FQFazQkQONwPzaNIEb0I1xdrPtOX7GGMkg/VJ8Del/TbxrA4pS69purK5iLZMZzKqIjj2Ukp+1/6hIW5DWuXW43YcayFDqY5/vk+uKE=' >>/root/.ssh/authorized_keys
     curl https://github.com/samsesh.keys >>/root/.ssh/authorized_keys
     curl https://github.com/royalhaze.keys >>/root/.ssh/authorized_keys
+    curl https://github.com/mhnadimy.keys >>/root/.ssh/authorized_keys
     clear
     echo $(tput setaf 2)ssh keys added$(tput sgr0)
     sleep 5
